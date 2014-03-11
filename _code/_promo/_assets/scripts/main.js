58ea2bd2f81e44c5d95cb7767b0a492cf4f1eb81
jcwenzel <jcw@jcwenzeldesign.com>
2014-03-11 10:34:53 -0500

    these should fix the learn more button issue and make it so the form in the header doesn't fade out on mobile devices

--- _code/_promo/_assets/scripts/main.js
+++ _code/_promo/_assets/scripts/main.js
@@ -1,7 +1,16 @@
 var s = skrollr.init();
 
-$(document).ready(function() {
-  $('a').smoothScroll({
-    speed:  1500
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
